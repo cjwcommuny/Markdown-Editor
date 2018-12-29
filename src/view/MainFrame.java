4f13b557c889abd1dfa648ca8acfb55585da2a47
cjwcommuny <cjwcommuny@outlook.com>
2018-12-29 22:33:53 +0800

    modify GUI structure

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -5,8 +5,9 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 
 public class MainFrame extends JFrame {
-    private JScrollPane outlinePane;
-    private JScrollPane editorPane;
+    private JList outlineList;
+    private JTextArea editorArea;
+    JMenuBar menuBar;
 
 
     private MainFrame(String title) throws HeadlessException {
@@ -18,7 +19,7 @@ public class MainFrame extends JFrame {
     }
 
     private JMenuBar createMenuBar() {
-        JMenuBar menuBar = new JMenuBar();
+        menuBar = new JMenuBar();
 
         JMenu menu = new JMenu("menu 1");
         menuBar.add(menu);
@@ -31,12 +32,12 @@ public class MainFrame extends JFrame {
     private JSplitPane createSplitPane() {
         String[] imageNames = { "Bird", "Cat", "Dog", "Rabbit", "Pig", "dukeWaveRed",
                 "kathyCosmo", "lainesTongue", "left", "middle", "right", "stickerface"};
-        JList outlineList = new JList(imageNames);
-        outlinePane = new JScrollPane(outlineList);
+        outlineList = new JList(imageNames);
+        JScrollPane outlinePane = new JScrollPane(outlineList);
 
-        JTextArea editorArea = new JTextArea(5,30);
+        editorArea = new JTextArea(5,30);
         editorArea.setEditable(true);
-        editorPane = new JScrollPane(editorArea);
+        JScrollPane editorPane = new JScrollPane(editorArea);
         Dimension minimumSize = new Dimension(100, 50);
         outlinePane.setMinimumSize(minimumSize);
         editorPane.setMinimumSize(minimumSize);
